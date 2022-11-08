45d18d11af33fae0a394d9c6dfa970d461543a94
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-11-07 22:39:09 -0500

    stuck at hashmap3 example

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -40,6 +40,34 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        match scores.get(&team_1_name) {
+            Some(score) => {
+                let team_1_score = team_1_score + score.goals_scored;
+            }
+            None => {}
+        }
+        match scores.get(&team_2_name) {
+            Some(score) => {
+                let team_2_score = team_2_score + score.goals_scored;
+            }
+            None => {}
+        }
+        scores.insert(
+            team_1_name.clone(),
+            Team {
+                name: team_1_name.clone(),
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            },
+        );
+        scores.insert(
+            team_2_name.clone(),
+            Team {
+                name: team_2_name.clone(),
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            },
+        );
     }
     scores
 }
