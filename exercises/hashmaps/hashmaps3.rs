6af834f86fb91e8363feb08735a7823d023a6744
Kwai Liu <[cmdrtomalak@gmail.com](mailto:cmdrtomalak@gmail.com)>
2022-11-20 16:28:48 -0500

    Done with hashmap

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -41,7 +39,64 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
 
-        let team1_score_prev = scores.get(&team_1_name);
+        match (scores.get(&team_1_name), scores.get(&team_2_name)) {
+            (Some(t1), Some(t2)) => {
+                let t1_next = Team {
+                    name: team_1_name.clone(),
+                    goals_scored: team_1_score + t1.goals_scored,
+                    goals_conceded: team_2_score + t1.goals_conceded,
+                };
+                let t2_next = Team {
+                    name: team_2_name.clone(),
+                    goals_scored: team_2_score + t2.goals_scored,
+                    goals_conceded: team_1_score + t2.goals_conceded,
+                };
+                scores.insert(team_1_name, t1_next);
+                scores.insert(team_2_name, t2_next);
+            }
+            (Some(t1), None) => {
+                let t1_next = Team {
+                    name: team_1_name.clone(),
+                    goals_scored: team_1_score + t1.goals_scored,
+                    goals_conceded: team_2_score + t1.goals_conceded,
+                };
+                let t2 = Team {
+                    name: team_2_name.clone(),
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                };
+                scores.insert(team_1_name, t1_next);
+                scores.insert(team_2_name, t2);
+            }
+            (None, Some(t2)) => {
+                let t1 = Team {
+                    name: team_1_name.clone(),
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                };
+                let t2_next = Team {
+                    name: team_2_name.clone(),
+                    goals_scored: team_2_score + t2.goals_scored,
+                    goals_conceded: team_1_score + t2.goals_conceded,
+                };
+                scores.insert(team_1_name, t1);
+                scores.insert(team_2_name, t2_next);
+            }
+            (None, None) => {
+                let t1 = Team {
+                    name: team_1_name.clone(),
+                    goals_scored: team_1_score,
+                    goals_conceded: team_2_score,
+                };
+                let t2 = Team {
+                    name: team_2_name.clone(),
+                    goals_scored: team_2_score,
+                    goals_conceded: team_1_score,
+                };
+                scores.insert(team_1_name, t1);
+                scores.insert(team_2_name, t2);
+            }
+        }
     }
     scores
 }
